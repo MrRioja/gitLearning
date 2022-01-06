f6dc981b608123f642996da39dccd617d816e5df
Luiz Rioja <lulyrioja@gmail.com>
2022-01-06 00:23:21 -0300

    :alien: Creating route for register user

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 const express = require("express");
 
+let users = [];
+
 const app = express();
 const routes = express.Router();
 
 app.use(express.json());
 
-routes.get("/start", (req, res) => {
-  return res.json({ message: "Hello" });
+routes.get("/users", (req, res) => {
+  return res.json(users);
+});
+
+routes.post("/user", (req, res) => {
+  const user = req.body;
+
+  users.push(user);
+
+  return res.json(user);
 });
 
 app.use(routes);
